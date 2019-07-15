b3dabf91a6d14d4d98e69145977d0cbb03631efa
Tiago Pires <tiago.pires@me.com>
2019-07-15 17:55:49 +1000

    Get posts using  query

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-
-
 <h1>latest News</h1>
 <ul>
 <? foreach($posts as $post) : ?>
     <li><?= $post->post_title ?>
-        <div><?= $post->post_content ?></div>
+        <div><?= $post -> post_content ?></div>
     </li>
 <? endforeach ?>
 </ul>
