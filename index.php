83d314bbedf36f0895b0baa811ceeaf6dd9ba39d
Tiago Pires <tiago.pires@me.com>
2019-07-15 20:47:57 +1000

    Added styles for resposive .cards

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <title>Tab Title</title>
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" />
     </head>
     <body>
         <h1>latest News</h1>
-        <ul>
+        <ul class="row">
             <? while (have_posts()) : the_post(); ?>
-                <li><? the_title() ?>
-                <? the_post_thumbnail(); ?>
-                <? the_content() ?>
+                <li class="column">
+                    <div class="card">
+                        <h2 class="card-title"><? the_title() ?></h2>
+                        <div class="card-image"><? the_post_thumbnail(); ?></div>
+                        <div class="card-content"><? the_excerpt() ?></div>
+                    </div>
                 </li>
             <? endwhile ?>
         </ul>
