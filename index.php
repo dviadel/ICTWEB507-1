af03f5a473656924ae409272a19b33b89a572933
Tiago Pires <tiago.pires@me.com>
2019-07-15 19:59:33 +1000

    Link stylesheet to theme

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-<h1>latest News</h1>
-<ul>
-	<? while (have_posts()) : the_post(); ?>
-        <li><? the_title() ?></li>
-        <? the_content() ?>
-	<? endwhile ?>
-</ul>
-
+<html>
+    <head>
+        <title>Tab Title</title>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" />
+    </head>
+    <body>
+        <h1>latest News</h1>
+        <ul>
+            <? while (have_posts()) : the_post(); ?>
+                <li><? the_title() ?>
+                <? the_post_thumbnail(); ?>
+                <? the_content() ?>
+                </li>
+            <? endwhile ?>
+        </ul>
+    </body>
+</html>
