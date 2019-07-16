034e60af0f7b7b6a3a5f116c09a51f5037d5f922
Tiago Pires <tiago.pires@me.com>
2019-07-16 20:45:27 +1000

    Add .card styles

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <head>
     <title>Foo News | News about the outer planet</title>
+    <link rel="stylesheet" href="http://foonews.local/wp-content/themes/foonews/style.css">
 </head>
 <body>
     <h1>Latest News</h1>
-    <ul class="naked-list">
+    <ul class="grid">
 
     <?php $x = 1; while($x <= 12) { ?>
 
-        <li>
+        <li class="grid-item">
             <article class="card">
                 <h2>Post Title</h2>
                 <img src="http://placehold.it/300x200">
